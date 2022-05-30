b4354df5f16bbb9e3b950d4b764bda96838d6be4
Ivan Tikhomirov <hakencontinuum1@gmail.com>
2022-05-30 22:47:43 +0200

    Added directory with the second Home Work

---
+++ <Tikhomirov>_<Ivan>_dz_2/<Tikhomirov>_<Ivan>_dz_2.3.py
@@ -0,0 +1,33 @@
+
+catalog = ['в', '5', 'часов', '17', 'минут', 'температура', 'воздуха', 'была', '+5', 'градусов']
+
+def is_digit(string):
+    if string.isdigit():
+        return True
+    else:
+        try:
+            float(string)
+            return True
+        except ValueError:
+            return False
+
+for item in catalog:
+    if is_digit(item):
+        if len(item) == 1:
+            catalog[catalog.index(item)] = ['"', f'0{item[-1]}', '"']
+        elif "+" in item:
+            catalog[catalog.index(item)] = ['"', f'+0{item[-1]}', '"']
+        else:
+            catalog[catalog.index(item)] = ['"', item, '"']
+
+for item in catalog:
+    if isinstance(item, str):
+        catalog[catalog.index(item)] = item
+    elif isinstance(item, list) == True:
+        j = catalog.index(item)
+        for i in item:
+            catalog.insert(j, i)
+        catalog.pop(j+3)
+
+print(' '.join(catalog))
+
